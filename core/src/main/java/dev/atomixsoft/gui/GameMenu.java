a43d749b9a596a31f546f4b972faafe445848442
ArcaneSunku <t4hnner@gmail.com>
2025-09-16 04:16:23 -0500

    Properly handles deselecting.

--- core/src/main/java/dev/atomixsoft/gui/GameMenu.java
+++ core/src/main/java/dev/atomixsoft/gui/GameMenu.java
@@ -15,16 +15,6 @@ public class GameMenu extends GroupAdapter {
 
         m_DeckChoice = new DeckChoice(m_Game);
         m_DeckChoice.setSize(64 * 5, 96);
-
-        addListener(new ClickListener() {
-            @Override
-            public void clicked(InputEvent event, float x, float y) {
-                super.clicked(event, x, y);
-
-                if(getChildren().contains(m_DeckChoice, false))
-                    m_DeckChoice.deselect();
-            }
-        });
     }
 
     @Override
@@ -38,6 +28,16 @@ public class GameMenu extends GroupAdapter {
         switch (m_State) {
             case DECK_SELECT -> {
                 addActor(m_DeckChoice);
+                getStage().addListener(new ClickListener() {
+                    @Override
+                    public void clicked(InputEvent event, float x, float y) {
+                        super.clicked(event, x, y);
+
+                        if(getChildren().contains(m_DeckChoice, false))
+                            m_DeckChoice.deselect();
+                    }
+                });
+
                 m_DeckChoice.setPosition((camera.viewportWidth - m_DeckChoice.getWidth()) / 2f, (camera.viewportHeight - m_DeckChoice.getHeight()) / 2f);
                 m_DeckChoice.update();
             }
@@ -51,6 +51,12 @@ public class GameMenu extends GroupAdapter {
                 // TODO: Handle SHOPPING state
             }
         }
+    }
+
+    @Override
+    protected void reset() {
+        super.reset();
+        clearListeners();
     }
 
     public void setState(GameState state) {
